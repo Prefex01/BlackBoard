9b3bf612abc5ae0687cf1d6d50789e848cc332ec
Sharkoon1 <skorpion72>
2017-04-01 19:17:29 +0200

    remove 2

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>Das Schwarzebrett-2</title>
+	<title>Das Schwarzebrett</title>
 	<link href="stylesheet.css" rel="stylesheet" type="text/css">
 </head>
 <body>
